a599a98c867678047278480619ffc9288298e151
Guido Princ <gprinc@itba.edu.ar>
2018-06-19 03:40:22 -0300

    Added notification

--- SmartHome/app/src/main/java/com/grupo7/hci/smarthome/smarthome/DoorActivity.java
+++ SmartHome/app/src/main/java/com/grupo7/hci/smarthome/smarthome/DoorActivity.java
@@ -1,9 +1,18 @@
 package com.grupo7.hci.smarthome.smarthome;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.provider.Settings;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
@@ -31,6 +40,12 @@ public class DoorActivity extends AppCompatActivity {
         final Switch switchOpen = (Switch) findViewById(R.id.switch_door_open);
         final Switch switchLock = (Switch) findViewById(R.id.switch_door_lock);
 
+        Intent notificationIntent = new Intent(context, DoorActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(DoorActivity.class);
+        stackBuilder.addNextIntent(notificationIntent);
+        final PendingIntent contentIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
         requestTag = ApiURLs.getInstance(context).executeAction(dev, "getState", new ArrayList(), new Response.Listener<JSONObject>() {
             @Override
@@ -43,6 +58,18 @@ public class DoorActivity extends AppCompatActivity {
                         if (status == "opened" || status == "opening") {
                             switchOpen.setText(R.string.open);
                             switchOpen.setChecked(true);
+                            Notification notification = new Notification.Builder(context)
+                                    .setContentTitle(getResources().getString(R.string.notifTitle))
+                                    .setContentText(getResources().getString(R.string.notifBody))
+                                    .setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 })
+                                    .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                                    .setLargeIcon(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_info_details))
+                                    .setSmallIcon(android.R.drawable.ic_menu_info_details)
+                                    .setContentIntent(contentIntent).build();
+
+                            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+                            notificationManager.notify(0, notification);
                         } else {
                             switchOpen.setText(R.string.close);
                         }
@@ -80,10 +107,23 @@ public class DoorActivity extends AppCompatActivity {
                 requestTag = ApiURLs.getInstance(context).executeAction(dev, actionName, new ArrayList(), new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        if (auxChecked)
+                        if (auxChecked) {
                             switchOpen.setText(R.string.open);
-                        else
+                            Notification notification = new Notification.Builder(context)
+                                    .setContentTitle(getResources().getString(R.string.notifTitle))
+                                    .setContentText(getResources().getString(R.string.notifBody))
+                                    .setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 })
+                                    .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                                    .setLargeIcon(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_info_details))
+                                    .setSmallIcon(android.R.drawable.ic_menu_info_details)
+                                    .setContentIntent(contentIntent).build();
+
+                            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+                            notificationManager.notify(0, notification);
+                        } else {
                             switchOpen.setText(R.string.close);
+                        }
                         // TODO add success toast
                     }
                 }, new Response.ErrorListener() {
@@ -129,34 +169,5 @@ public class DoorActivity extends AppCompatActivity {
         ApiURLs.getInstance(context).cancelRequest(requestTag);
     }
 
-    protected void onChangeOpenStatus() {
-//        $("#open-switch").on("click", function() {
-//            var status = $("#open-status").text();
-//            if(status === "Open") {
-//                api.device.executeAction(device.id, "close", [])
-//        .done(function(data, textStatus, jqXHR) {
-//                    $("#open-status").text("Closed");
-//                })
-//        else {
-//            api.device.executeAction(device.id, "open", [])
-//        .done(function(data, textStatus, jqXHR) {
-//                $("#open-status").text("Open");
-//            })
-    }
-
-    protected void onChangeLockStatus() {
-//        $("#lock-switch").on("click", function() {
-//            var status = $("#lock-status").text();
-//            if(status === "Locked") {
-//                api.device.executeAction(device.id, "unlock", [])
-//        .done(function(data, textStatus, jqXHR) {
-//                    $("#lock-status").text("Unlocked");
-//                })
-//        else {
-//            api.device.executeAction(device.id, "lock", [])
-//        .done(function(data, textStatus, jqXHR) {
-//                $("#lock-status").text("Locked");
-//            })
-    }
 
 }
